7ed1ed699c1931a3cc2a76cf8ac029aa3d235472
browniebraun <browniebraun@860744bd-22fc-0310-8c96-e9fe5004b5ca>
2010-05-03 18:49:17 +0000

    - fix: sort list of translated language names alphabetically

--- cacti/branches/main/lib/ajax/get_languages.php
+++ cacti/branches/main/lib/ajax/get_languages.php
@@ -85,6 +85,8 @@ $location = str_replace(array( "?" . $search . "&", "?" . $search, "&" . $search
 $location .= (strpos($location, '?')) ? '&' : '?';
 
 if(sizeof($supported_languages["cacti"])>0) {
+	/* sort list translated names alphabetically */
+	sort($supported_languages["cacti"]);
 	foreach($supported_languages["cacti"] as $lang) {
 		?><h6><a href="<?php print $location . "language=" . $locations[$lang]["locale"]; ?>"><img src="<?php echo CACTI_URL_PATH; ?>images/flag_icons/<?php print $locations[$lang]["flag"];?>.gif" align="bottom" width="16" height="11">&nbsp;<?php print $locations[$lang]["language"];?></a></h6><?php
 	}
