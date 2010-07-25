eeeaa1508a08ec1b9abcb23d1febe449b4141146
thewitness <thewitness@860744bd-22fc-0310-8c96-e9fe5004b5ca>
2010-07-25 00:42:11 +0000

    QA: final QA onsite.  Fixing continued issues with mutex locking QA: usleep value was too large.  Caused pollers to overrun on large systems.

--- spine/branches/main/spine.c
+++ spine/branches/main/spine.c
@@ -135,7 +135,7 @@ int main(int argc, char *argv[]) {
 	int poller_counter = 0;
 	int last_active_threads = 0;
 	int valid_conf_file = FALSE;
-	long int EXTERNAL_THREAD_SLEEP = 5000;
+	long int EXTERNAL_THREAD_SLEEP = 50;
 	long int internal_thread_sleep;
 	char querybuf[BIG_BUFSIZE], *qp = querybuf;
 	char *device_time = NULL;
@@ -599,10 +599,8 @@ int main(int argc, char *argv[]) {
 	
 						/* wait for the child to read and process the structure */
 						while (!thread_ready) { 
-							usleep(100000);
+							usleep(internal_thread_sleep);
 						}
-
-						thread_mutex_unlock(LOCK_THREAD);
 
 						SPINE_LOG_DEBUG(("DEBUG: The Value of Active Threads is %i", active_threads));
 	
@@ -621,6 +619,8 @@ int main(int argc, char *argv[]) {
 						break;
 				}
 	
+				thread_mutex_unlock(LOCK_THREAD);
+
 				/* get current time and exit program if time limit exceeded */
 				if (poller_counter >= 20) {
 					current_time = get_time_as_double();
