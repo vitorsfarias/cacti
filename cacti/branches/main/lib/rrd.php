00df2d808a73faae9fe36824e87dd18863541fe1
gandalf <gandalf@860744bd-22fc-0310-8c96-e9fe5004b5ca>
2010-12-07 20:56:59 +0000

    fixing initialization errors

--- cacti/branches/main/lib/rrd.php
+++ cacti/branches/main/lib/rrd.php
@@ -374,7 +374,7 @@ function rrdtool_function_fetch($local_data_id, $start_time, $end_time, $resolut
 	global $rrd_fetch_cache;
 
 	if (empty($local_data_id)) {
-		unset($var);
+		$var = array();
 		return $var;
 	}
 
