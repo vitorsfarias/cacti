6418335e75dcd4320ea3393c097ec263cefd0b80
gandalf <gandalf@860744bd-22fc-0310-8c96-e9fe5004b5ca>
2010-12-07 20:26:18 +0000

    missing include

--- cacti/branches/main/lib/graph/graphs_form.php
+++ cacti/branches/main/lib/graph/graphs_form.php
@@ -609,6 +609,7 @@ function graph_item() {
 function graph_diff() {
 	global $colors;
 	require(CACTI_BASE_PATH . "/include/presets/preset_rra_arrays.php");
+	require(CACTI_BASE_PATH . "/include/graph/graph_arrays.php");
 	require_once(CACTI_BASE_PATH . "/lib/graph/graph_info.php");
 
 	/* ================= input validation ================= */
