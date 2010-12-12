e6f95d0af4bbdcf5994b74d38f8fc3b78ddbf8a7
thewitness <thewitness@860744bd-22fc-0310-8c96-e9fe5004b5ca>
2010-12-12 17:08:22 +0000

    QA: Double to Single as a standard.

--- cacti/branches/main/include/top_graph_header.php
+++ cacti/branches/main/include/top_graph_header.php
@@ -99,37 +99,37 @@ $page_title = api_plugin_hook_function('page_title', draw_navigation_text("title
 		}
 	}
 	?>
-	<meta http-equiv="Content-Type" content="text/html;charset=utf-8" >
-	<meta http-equiv="Content-Script-Type" content="text/javascript" >
-	<meta http-equiv="Content-Style-Type" content="text/css">
-	<link type="text/css" href="<?php echo CACTI_URL_PATH; ?>include/main.css" rel="stylesheet">
-	<link type="text/css" href="<?php echo CACTI_URL_PATH; ?>include/dd.css" rel="stylesheet">
-	<link type="text/css" href="<?php echo CACTI_URL_PATH; ?>include/jquery.autocomplete.css" rel="stylesheet">
-	<link type="text/css" media="screen" href="<?php echo CACTI_URL_PATH; ?>include/css/colorpicker.css" rel="stylesheet">
-	<link type="text/css" media="screen" href="<?php echo CACTI_URL_PATH; ?>include/css/cacti_dd_menu.css" rel="stylesheet">
-	<link type="text/css" media="screen" href="<?php echo CACTI_URL_PATH; ?>include/css/jquery-ui.css" rel="stylesheet">
-	<link href="<?php echo CACTI_URL_PATH; ?>images/favicon.ico" rel="shortcut icon">
-	<script type="text/javascript" src="<?php echo CACTI_URL_PATH; ?>include/js/jquery/jquery.js"></script>
-	<script type="text/javascript" src="<?php echo CACTI_URL_PATH; ?>include/js/jquery/jquery-ui.js"></script>
-	<script type="text/javascript" src="<?php echo CACTI_URL_PATH; ?>include/js/layout.js"></script>
-	<script type="text/javascript" src="<?php echo CACTI_URL_PATH; ?>include/layout.php"></script>
-	<script type="text/javascript" src="<?php echo CACTI_URL_PATH; ?>include/js/jstree/jquery.tree.js"></script>
-	<script type="text/javascript" src="<?php echo CACTI_URL_PATH; ?>include/js/jquery/jquery.cookie.js"></script>
-	<script type="text/javascript" src="<?php echo CACTI_URL_PATH; ?>include/js/jquery/jquery.autocomplete.js"></script>
-	<script type="text/javascript" src="<?php echo CACTI_URL_PATH; ?>include/js/jquery/jquery.bgiframe.js"></script>
-	<script type="text/javascript" src="<?php echo CACTI_URL_PATH; ?>include/js/jquery/jquery.ajaxQueue.js"></script>
-	<script type="text/javascript" src="<?php echo CACTI_URL_PATH; ?>include/js/jquery/jquery.tablednd.js"></script>
-	<script type="text/javascript" src="<?php echo CACTI_URL_PATH; ?>include/js/jquery/jquery.dropdown.js"></script>
-	<script type="text/javascript" src="<?php echo CACTI_URL_PATH; ?>include/js/jquery/jquery.dd.js"></script>
-	<script type="text/javascript" src="<?php echo CACTI_URL_PATH; ?>include/js/jquery/colorpicker.js"></script>
-	<script type="text/javascript" src="<?php echo CACTI_URL_PATH; ?>include/js/jscalendar/calendar.js"></script>
-	<script type="text/javascript" src="<?php echo CACTI_URL_PATH; ?>include/js/jscalendar/lang/<?php print (read_config_option('i18n_language_support') != 0) ? CACTI_LANGUAGE_FILE : "english_usa";?>.js"></script>
-	<script type="text/javascript" src="<?php echo CACTI_URL_PATH; ?>include/js/jscalendar/calendar-setup.js"></script>
-	<script type="text/javascript" src="<?php echo CACTI_URL_PATH; ?>include/js/jstree/plugins/jquery.tree.cookie.js"></script>
+	<meta http-equiv='Content-Type' content='text/html;charset=utf-8'>
+	<meta http-equiv='Content-Script-Type' content='text/javascript'>
+	<meta http-equiv='Content-Style-Type' content='text/css'>
+	<link type='text/css' href='<?php echo CACTI_URL_PATH; ?>include/main.css' rel='stylesheet'>
+	<link type='text/css' href='<?php echo CACTI_URL_PATH; ?>include/dd.css' rel='stylesheet'>
+	<link type='text/css' href='<?php echo CACTI_URL_PATH; ?>include/jquery.autocomplete.css' rel='stylesheet'>
+	<link type='text/css' media='screen' href='<?php echo CACTI_URL_PATH; ?>include/css/colorpicker.css' rel='stylesheet'>
+	<link type='text/css' media='screen' href='<?php echo CACTI_URL_PATH; ?>include/css/cacti_dd_menu.css' rel='stylesheet'>
+	<link type='text/css' media='screen' href='<?php echo CACTI_URL_PATH; ?>include/css/jquery-ui.css' rel='stylesheet'>
+	<link href='<?php echo CACTI_URL_PATH; ?>images/favicon.ico' rel='shortcut icon'>
+	<script type='text/javascript' src='<?php echo CACTI_URL_PATH; ?>include/js/jquery/jquery.js'></script>
+	<script type='text/javascript' src='<?php echo CACTI_URL_PATH; ?>include/js/jquery/jquery-ui.js'></script>
+	<script type='text/javascript' src='<?php echo CACTI_URL_PATH; ?>include/js/layout.js'></script>
+	<script type='text/javascript' src='<?php echo CACTI_URL_PATH; ?>include/layout.php'></script>
+	<script type='text/javascript' src='<?php echo CACTI_URL_PATH; ?>include/js/jstree/jquery.tree.js'></script>
+	<script type='text/javascript' src='<?php echo CACTI_URL_PATH; ?>include/js/jquery/jquery.cookie.js'></script>
+	<script type='text/javascript' src='<?php echo CACTI_URL_PATH; ?>include/js/jquery/jquery.autocomplete.js'></script>
+	<script type='text/javascript' src='<?php echo CACTI_URL_PATH; ?>include/js/jquery/jquery.bgiframe.js'></script>
+	<script type='text/javascript' src='<?php echo CACTI_URL_PATH; ?>include/js/jquery/jquery.ajaxQueue.js'></script>
+	<script type='text/javascript' src='<?php echo CACTI_URL_PATH; ?>include/js/jquery/jquery.tablednd.js'></script>
+	<script type='text/javascript' src='<?php echo CACTI_URL_PATH; ?>include/js/jquery/jquery.dropdown.js'></script>
+	<script type='text/javascript' src='<?php echo CACTI_URL_PATH; ?>include/js/jquery/jquery.dd.js'></script>
+	<script type='text/javascript' src='<?php echo CACTI_URL_PATH; ?>include/js/jquery/colorpicker.js'></script>
+	<script type='text/javascript' src='<?php echo CACTI_URL_PATH; ?>include/js/jscalendar/calendar.js'></script>
+	<script type='text/javascript' src='<?php echo CACTI_URL_PATH; ?>include/js/jscalendar/lang/<?php print (read_config_option('i18n_language_support') != 0) ? CACTI_LANGUAGE_FILE : "english_usa";?>.js'></script>
+	<script type='text/javascript' src='<?php echo CACTI_URL_PATH; ?>include/js/jscalendar/calendar-setup.js'></script>
+	<script type='text/javascript' src='<?php echo CACTI_URL_PATH; ?>include/js/jstree/plugins/jquery.tree.cookie.js'></script>
 	<?php initializeCookieVariable(); api_plugin_hook('page_head'); ?>
 </head>
 <body class='body'>
-<script type="text/javascript" src="<?php echo CACTI_URL_PATH; ?>include/js/wztooltip/wz_tooltip.js"></script>
+<script type='text/javascript' src='<?php echo CACTI_URL_PATH; ?>include/js/wztooltip/wz_tooltip.js'></script>
 <div id='header'>
 	<div id='logobar'></div>
 	<div id='navbar'>
@@ -152,30 +152,30 @@ $page_title = api_plugin_hook_function('page_title', draw_navigation_text("title
 		</div>
 	</div>
 	<div id='navbrcrumb'>
-		<div style='float:left'>
+		<div id='brcrumb' style='float:left'>
 			<?php echo draw_navigation_text();?>
 		</div>
 		<div style='float:right'>
-			<a href="<?php echo cacti_wiki_url();?>" target="_blank">
-			<img src="<?php echo CACTI_URL_PATH; ?>images/help.gif" title="<?php print __("Help");?>" alt="<?php print __("Help");?>" align="top">
+			<a href='<?php echo cacti_wiki_url();?>' target='_blank'>
+			<img src='<?php echo CACTI_URL_PATH; ?>images/help.gif' title='<?php print __("Help");?>' alt='<?php print __("Help");?>' align='top'>
 			</a>
 		</div>
 		<div style='float:right'>
 		<?php	if (read_config_option("auth_method") != AUTH_METHOD_NONE) {
 					if(read_config_option('i18n_timezone_support') != 0) {
-						?><a href="#" id="menu_timezones" rel="<?php echo CACTI_URL_PATH; ?>"><span id="date_time_format"><strong><?php echo __date("D, " . date_time_format() . " T");?></strong></span></a><?php
+						?><a href='#' id='menu_timezones' rel='<?php echo CACTI_URL_PATH; ?>'><span id='date_time_format'><strong><?php echo __date("D, " . date_time_format() . " T");?></strong></span></a><?php
 					}else {
-						?><span id="date_time_format"><strong><?php echo __date("D, " . date_time_format() . " T");?></strong></span><?php
+						?><span id='date_time_format'><strong><?php echo __date("D, " . date_time_format() . " T");?></strong></span><?php
 					}
 				?>
-					&nbsp;&nbsp;&nbsp;<?php print __("Logged in as");?> <strong><?php print db_fetch_cell("select username from user_auth where id=" . $_SESSION["sess_user_id"]);?></strong> (<a href="<?php echo CACTI_URL_PATH; ?>logout.php"><?php print __("Logout");?></a>)
+					&nbsp;&nbsp;&nbsp;<?php print __("Logged in as");?> <strong><?php print db_fetch_cell("select username from user_auth where id=" . $_SESSION["sess_user_id"]);?></strong> (<a href='<?php echo CACTI_URL_PATH; ?>logout.php'><?php print __("Logout");?></a>)
 		<?php } ?>
 		</div>
 		<?php if(read_config_option('i18n_language_support') != 0) {?>
 		<div style='float:right;'>
-			<a href="#" id="menu_languages" rel="<?php echo CACTI_URL_PATH; ?>"><img src="<?php echo CACTI_URL_PATH; ?>images/flag_icons/<?php print CACTI_COUNTRY;?>.gif" alt="<?php print CACTI_COUNTRY;?>" align="top">&nbsp;<?php print $lang2locale[CACTI_LOCALE]["language"];?></a>
+			<a href='#' id='menu_languages' rel='<?php echo CACTI_URL_PATH; ?>'><img src='<?php echo CACTI_URL_PATH; ?>images/flag_icons/<?php print CACTI_COUNTRY;?>.gif' alt='<?php print CACTI_COUNTRY;?>' align='top'>&nbsp;<?php print $lang2locale[CACTI_LOCALE]["language"];?></a>
 		</div>
-		<div id="loading" style="display:none; float:right"><img src="<?php echo CACTI_URL_PATH; ?>images/load_small.gif" align="top" alt="<?php print __("loading");?>">LOADING</div>
+		<div id='loading' style='display:none; float:right'><img src='<?php echo CACTI_URL_PATH; ?>images/load_small.gif' align='top' alt='<?php print __("loading");?>'>LOADING</div>
 		<?php }?>
 	</div>
 </div>
@@ -185,7 +185,7 @@ $page_title = api_plugin_hook_function('page_title', draw_navigation_text("title
 		<div id='tree_filter'>
 			<?php graph_view_tree_filter();?>
 		</div>
-		<div class="tree"></div>
+		<div class='tree'></div>
 	</div>
 	<div id='vsplitter' onMouseout='doneDivResize()' onMouseover='doDivResize(this,event)' onMousemove='doDivResize(this,event)'>
 		<div id='vsplitter_toggle' onClick='vSplitterToggle()' onMouseover='vSplitterEm()' onMouseout='vSplitterUnEm()' title='<?php print __("Hide/Unhide Menu");?>'></div>
