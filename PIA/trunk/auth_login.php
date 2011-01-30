c0141c5144c0aa238cdd7bf08a2e5ca3f79236ee
thewitness <thewitness@860744bd-22fc-0310-8c96-e9fe5004b5ca>
2011-01-30 14:53:46 +0000

    QA: One additional option to support new loginmod plugin.

--- PIA/trunk/auth_login.php
+++ PIA/trunk/auth_login.php
@@ -264,7 +264,7 @@ function auth_display_custom_error_message($message) {
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
-	<title>Login to Cacti</title>
+	<title><?php print read_config_option("loginmod_title");?></title>
 	<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
 	<STYLE TYPE="text/css">
 	<!--
