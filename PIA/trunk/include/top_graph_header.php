d03557bf4d254477de4933c2f49ab5280e59c90e
thewitness <thewitness@860744bd-22fc-0310-8c96-e9fe5004b5ca>
2011-03-16 00:33:07 +0000

    QA: Zoom refresh not functional

--- PIA/trunk/include/top_graph_header.php
+++ PIA/trunk/include/top_graph_header.php
@@ -64,7 +64,7 @@ $page_title = api_plugin_hook_function('page_title', draw_navigation_text("title
 	<?php
 	if (isset($_SESSION["custom"]) && $_SESSION["custom"] == true) {
 		print "<meta http-equiv=refresh content='99999'>\r\n";
-	}else if (isset($_SESSION["action"]) && $_SESSION["action"] == 'zoom') {
+	}else if (isset($_REQUEST["action"]) && $_REQUEST["action"] == 'zoom') {
 		print "<meta http-equiv=refresh content='99999'>\r\n";
 	}else{
 		$refresh = api_plugin_hook_function('top_graph_refresh', htmlspecialchars(read_graph_config_option("page_refresh"),ENT_QUOTES));
