4785f548aa0fd5f3cfbd2a346f86e3b0c0eb6af5
thewitness <thewitness@860744bd-22fc-0310-8c96-e9fe5004b5ca>
2011-05-06 01:35:40 +0000

    -bug#0001989: IE9 breaks Cacti when objects are hidden using 'display:none;' style tag

--- cacti/branches/main/include/top_header.php
+++ cacti/branches/main/include/top_header.php
@@ -35,6 +35,7 @@ load_current_session_value("toptab", "sess_cacti_toptab", "console");
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
+	<meta http-equiv="X-UA-Compatible" content="IE=EmulateIE8">
 	<title><?php echo $page_title; ?></title>
 	<meta http-equiv='Content-Type' content='text/html;charset=utf-8'>
 	<meta http-equiv='Content-Script-Type' content='text/javascript'>
