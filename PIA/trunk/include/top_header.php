47aeac6ac39d82d217b62c9d751943a30f1aedd1
thewitness <thewitness@860744bd-22fc-0310-8c96-e9fe5004b5ca>
2011-05-06 01:35:40 +0000

    -bug#0001989: IE9 breaks Cacti when objects are hidden using 'display:none;' style tag

--- PIA/trunk/include/top_header.php
+++ PIA/trunk/include/top_header.php
@@ -35,6 +35,7 @@ $page_title = api_plugin_hook_function('page_title', draw_navigation_text("title
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
+	<meta http-equiv="X-UA-Compatible" content="IE=EmulateIE8">
 	<title><?php echo $page_title; ?></title>
 	<link href="<?php echo $config['url_path']; ?>include/main.css" type="text/css" rel="stylesheet">
 	<link href="<?php echo $config['url_path']; ?>images/favicon.ico" rel="shortcut icon">
