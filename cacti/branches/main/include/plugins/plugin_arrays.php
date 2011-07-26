f0bdd0b128c552ed6d9f354d387e8f24d95c8d7e
gandalf <gandalf@860744bd-22fc-0310-8c96-e9fe5004b5ca>
2011-07-26 20:08:53 +0000

    Plugin Management: rough and fast implementation of PIA3.0 ordering is not functional ATM ALL plugins will fail and cause the web page to break as they are mostly using "old" $config variables
    Using new table class ;-)

---
+++ cacti/branches/main/include/plugins/plugin_arrays.php
@@ -0,0 +1,51 @@
+<?php
+/*
+ +-------------------------------------------------------------------------+
+ | Copyright (C) 2004-2011 The Cacti Group                                 |
+ |                                                                         |
+ | This program is free software; you can redistribute it and/or           |
+ | modify it under the terms of the GNU General Public License             |
+ | as published by the Free Software Foundation; either version 2          |
+ | of the License, or (at your option) any later version.                  |
+ |                                                                         |
+ | This program is distributed in the hope that it will be useful,         |
+ | but WITHOUT ANY WARRANTY; without even the implied warranty of          |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           |
+ | GNU General Public License for more details.                            |
+ +-------------------------------------------------------------------------+
+ | Cacti: The Complete RRDTool-based Graphing Solution                     |
+ +-------------------------------------------------------------------------+
+ | This code is designed, written, and maintained by the Cacti Group. See  |
+ | about.php and/or the AUTHORS file for specific developer information.   |
+ +-------------------------------------------------------------------------+
+ | http://www.cacti.net/                                                   |
+ +-------------------------------------------------------------------------+
+*/
+
+require_once(CACTI_BASE_PATH . "/include/plugins/plugin_constants.php");
+
+$plugin_actions = array(
+	PLUGIN_ACTION_INSTALL 	=> __("Install"),
+	PLUGIN_ACTION_ENABLE 	=> __("Enable"),
+	PLUGIN_ACTION_DISABLE 	=> __("Disable"),
+	PLUGIN_ACTION_UNINSTALL => __("Uninstall"),
+//	PLUGIN_ACTION_CHECK 	=> __("Check)"
+	);
+
+	
+$plugin_status_names = array(
+	PLUGIN_STATUS_DISABLED 					=> __('Disabled'),
+	PLUGIN_STATUS_ACTIVE_1 					=> __('Active'),
+	PLUGIN_STATUS_NOT_INSTALLED 			=> __('Not Installed'),
+	PLUGIN_STATUS_ACTIVE_2 					=> __('Active'),
+	PLUGIN_STATUS_AWAITING_CONFIGURATION 	=> __('Awaiting Configuration'),
+	PLUGIN_STATUS_AWAITING_UPGRADE 			=> __('Awaiting Upgrade'),
+	PLUGIN_STATUS_INSTALLED 				=> __('Installed')
+	);
+	
+if (!isset($plugins) || !is_array($plugins)) {
+	$plugins = array();
+}
+$plugin_hooks = array();
+$plugins_system = array('settings', 'boost', 'dsstats');
+	
---
+++ cacti/branches/main/include/plugins/plugin_constants.php
@@ -0,0 +1,40 @@
+<?php
+/*
+ +-------------------------------------------------------------------------+
+ | Copyright (C) 2004-2011 The Cacti Group                                 |
+ |                                                                         |
+ | This program is free software; you can redistribute it and/or           |
+ | modify it under the terms of the GNU General Public License             |
+ | as published by the Free Software Foundation; either version 2          |
+ | of the License, or (at your option) any later version.                  |
+ |                                                                         |
+ | This program is distributed in the hope that it will be useful,         |
+ | but WITHOUT ANY WARRANTY; without even the implied warranty of          |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           |
+ | GNU General Public License for more details.                            |
+ +-------------------------------------------------------------------------+
+ | Cacti: The Complete RRDTool-based Graphing Solution                     |
+ +-------------------------------------------------------------------------+
+ | This code is designed, written, and maintained by the Cacti Group. See  |
+ | about.php and/or the AUTHORS file for specific developer information.   |
+ +-------------------------------------------------------------------------+
+ | http://www.cacti.net/                                                   |
+ +-------------------------------------------------------------------------+
+*/
+
+/* used for plugin actions */
+define("PLUGIN_ACTION_INSTALL", "Install");
+define("PLUGIN_ACTION_ENABLE", "Enable");
+define("PLUGIN_ACTION_DISABLE", "Disable");
+define("PLUGIN_ACTION_UNINSTALL", "Uninstall");
+define("PLUGIN_ACTION_CHECK", "Check");
+
+/* plugin status names */
+define("PLUGIN_STATUS_DISABLED", "-2");
+define("PLUGIN_STATUS_ACTIVE_1", "-1");
+define("PLUGIN_STATUS_NOT_INSTALLED", "0");
+define("PLUGIN_STATUS_ACTIVE_2", "1");
+define("PLUGIN_STATUS_AWAITING_CONFIGURATION", "2");
+define("PLUGIN_STATUS_AWAITING_UPGRADE", "3");
+define("PLUGIN_STATUS_INSTALLED", "4");
+
