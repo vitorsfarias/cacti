60e646d9f05fef13a44d9699f5d4d40d8682bfaf
gandalf <gandalf@860744bd-22fc-0310-8c96-e9fe5004b5ca>
2011-08-03 17:52:14 +0000

    fix broken export

--- cacti/branches/main/templates_export.php
+++ cacti/branches/main/templates_export.php
@@ -25,27 +25,34 @@
 include("./include/auth.php");
 include_once(CACTI_BASE_PATH . "/lib/export.php");
 
+/* define global constants to rule available options */
+define("EXPORT_OUTPUT_BROWSER", 1);
+define("EXPORT_OUTPUT_RAW_XML", 2);
+define("EXPORT_OUTPUT_LOCAL_FILE", 3);
+
 /* set default action */
 if (!isset($_REQUEST["action"])) { $_REQUEST["action"] = ""; }
 
 switch (get_request_var_request("action")) {
+	/* the alternate save function changes the "save" action to a "create" action
+	 * so be nice and handle both the same way */
 	case 'save':
+	case 'create':
 		form_save();
 
 		break;
 	default:
 		include_once(CACTI_BASE_PATH . "/include/top_header.php");
-
+		/* display the export functions available */
 		export();
 
 		include_once(CACTI_BASE_PATH . "/include/bottom_footer.php");
 		break;
 }
 
-/* --------------------------
-    The Save Function
-   -------------------------- */
-
+/**
+ * The Save Function
+ */
 function form_save() {
 	global $export_types, $export_errors;
 
@@ -57,15 +64,15 @@ function form_save() {
 		$export_errors = 0;
 		$xml_data = get_item_xml($_POST["export_type"], $_POST["export_item_id"], (((isset($_POST["include_deps"]) ? $_POST["include_deps"] : "") == "") ? false : true));
 
-		if (get_request_var_post("output_format") == "1") {
+		if (get_request_var_post("output_format") == EXPORT_OUTPUT_BROWSER) {
 			include_once(CACTI_BASE_PATH . "/include/top_header.php");
-			print "<table class='wp100 center'><tr><td><pre>" . htmlspecialchars($xml_data) . "</pre></td></tr></table>";
+			print "<table class='wp100 left'><tr><td><pre>" . htmlspecialchars($xml_data) . "</pre></td></tr></table>";
 			include_once(CACTI_BASE_PATH . "/include/bottom_footer.php");
-		}elseif (get_request_var_post("output_format") == "2") {
+		}elseif (get_request_var_post("output_format") == EXPORT_OUTPUT_RAW_XML) {
 			header("Content-type: application/xml");
-			if ($export_errors) echo "WARNING: Export Errors Encountered.  Refresh Browser Window for Details!\n";
+			if ($export_errors) echo __("WARNING: Export Errors Encountered. Refresh Browser Window for Details!") . "\n";
 			print $xml_data;
-		}elseif (get_request_var_post("output_format") == "3") {
+		}elseif (get_request_var_post("output_format") == EXPORT_OUTPUT_LOCAL_FILE) {
 			if ($export_errors) {
 				header("Location: templates_export.php");
 			}else{
@@ -77,10 +84,9 @@ function form_save() {
 	}
 }
 
-/* ---------------------------
-    Template Export Functions
-   --------------------------- */
-
+/**
+ * Template Export Functions
+ */
 function export() {
 	global $export_types;
 
@@ -89,6 +95,7 @@ function export() {
 		$_REQUEST["export_type"] = "graph_template";
 	}
 
+	/* now create the page to select the type of template */
 	?>
 	<form name="form_graph_id" action="templates_export.php">
 	<table class='topBoxAlt'>
@@ -112,7 +119,10 @@ function export() {
 
 	html_start_box("<strong>" . __("Export Template") . "</strong> [" . $export_types{get_request_var_request("export_type")}["name"] . "]", "100", "3", "center", "");
 
-	form_alternate_row_color("item"); ?>
+	form_alternate_row_color("item"); 
+	
+	/* draw all available objects of the type chosen above */
+	?>
 		<td width="50%">
 			<font class="textEditTitle"><?php print $export_types{$_REQUEST["export_type"]}["name"];?> <?php print __("to Export");?></font><br>
 			<?php print __("Choose the exact item to export to XML.");?>
@@ -122,7 +132,10 @@ function export() {
 		</td>
 	</tr>
 
-	<?php form_alternate_row_color("dependencies"); ?>
+	<?php 
+	/* want dependencies included? */
+	form_alternate_row_color("dependencies"); 
+	?>
 		<td width="50%">
 			<font class="textEditTitle"><?php print __("Include Dependencies");?></font><br>
 			<?php print __("Some templates rely on other items in Cacti to function properly. It is highly recommended that you select this box or the resulting import may fail.");?>
@@ -132,16 +145,20 @@ function export() {
 		</td>
 	</tr>
 
-	<?php form_alternate_row_color("format"); ?>
+	<?php 
+
+	/* were do you want to see the output? */
+	form_alternate_row_color("format"); 
+	?>
 		<td width="50%">
 			<font class="textEditTitle"><?php print __("Output Format");?></font><br>
 			<?php print __("Choose the format to output the resulting XML file in.");?>
 		</td>
 		<td>
 			<?php
-			form_radio_button("output_format", "3", "1", __("Output to the Browser (within Cacti)"),"1",true); print "<br>";
-			form_radio_button("output_format", "3", "2", __("Output to the Browser (raw XML)"),"1",true); print "<br>";
-			form_radio_button("output_format", "3", "3", __("Save File Locally"),"1",true);
+			form_radio_button("output_format", "3", EXPORT_OUTPUT_BROWSER, __("Output to the Browser (within Cacti)"),"1",true); print "<br>";
+			form_radio_button("output_format", "3", EXPORT_OUTPUT_RAW_XML, __("Output to the Browser (raw XML)"),"1",true); print "<br>";
+			form_radio_button("output_format", "3", EXPORT_OUTPUT_LOCAL_FILE, __("Save File Locally"),"1",true);
 			?>
 		</td>
 	</tr>
@@ -152,5 +169,6 @@ function export() {
 	form_hidden_box("export_type", get_request_var_request("export_type"), "");
 	form_hidden_box("save_component_export","1","");
 
+	/* display a nice submit button that reads "export" */
 	form_save_button_alt("", "save", "export");
 }
