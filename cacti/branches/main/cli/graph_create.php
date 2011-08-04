dfb0c5bde5d83e58d395bcaca13287b427b0013c
rony <rony@860744bd-22fc-0310-8c96-e9fe5004b5ca>
2011-08-04 02:24:18 +0000

    Rename lib/api_automation_tools.php to lib/automation_tools.php

--- cacti/branches/main/cli/graph_create.php
+++ cacti/branches/main/cli/graph_create.php
@@ -33,7 +33,7 @@ $no_http_headers = true;
 include(dirname(__FILE__)."/../include/global.php");
 require_once(CACTI_BASE_PATH . "/include/data_query/data_query_constants.php");
 require_once(CACTI_BASE_PATH . "/include/device/device_constants.php");
-include_once(CACTI_BASE_PATH."/lib/api_automation_tools.php");
+include_once(CACTI_BASE_PATH."/lib/automation_tools.php");
 include_once(CACTI_BASE_PATH."/lib/data_query.php");
 include_once(CACTI_BASE_PATH."/lib/utility.php");
 include_once(CACTI_BASE_PATH."/lib/sort.php");
