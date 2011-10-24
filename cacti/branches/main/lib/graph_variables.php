240fffebeabdd77ef3b0c78b679c8887ab7b2b6f
rony <rony@860744bd-22fc-0310-8c96-e9fe5004b5ca>
2011-10-24 20:58:42 +0000

    Bandwidth summation fails if NAN values are present

--- cacti/branches/main/lib/graph_variables.php
+++ cacti/branches/main/lib/graph_variables.php
@@ -227,8 +227,8 @@ function bandwidth_summation($local_data_id, $start_time, $end_time, $rra_steps,
 		if (isset($fetch_array["values"][$i])) {
 			$values_array = $fetch_array["values"][$i];
 
-			for ($j=0;$j<count($fetch_array["values"][$i]);$j++) {
-				$sum += $fetch_array["values"][$i][$j];
+			foreach ($fetch_array["values"][$i] as $value) {
+				$sum += $value;
 			}
 
 			if (count($fetch_array["values"][$i]) != 0) {
