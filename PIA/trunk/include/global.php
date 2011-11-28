4837a2286ee84e729a9daf77f5830d8a263ca693
rony <rony@860744bd-22fc-0310-8c96-e9fe5004b5ca>
2011-11-28 02:05:19 +0000

    Bring PIA inline with 0.8.7i release

--- PIA/trunk/include/global.php
+++ PIA/trunk/include/global.php
@@ -230,6 +230,6 @@ include_once($config["library_path"] . "/auth.php");
 api_plugin_hook("config_insert");
 
 /* current cacti version */
-$config["cacti_version"] = "0.8.7h";
+$config["cacti_version"] = "0.8.7i";
 
 ?>
