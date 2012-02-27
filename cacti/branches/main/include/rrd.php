758961231bc626f6b90bd106ae9e28830e7b7131
cigamit <cigamit@860744bd-22fc-0310-8c96-e9fe5004b5ca>
2012-02-27 08:50:53 +0000

    Partial fix for broken graphs from the mass single quote replacements.  This tracking down other issues

--- cacti/branches/main/include/rrd.php
+++ cacti/branches/main/include/rrd.php
@@ -22,7 +22,7 @@
  +-------------------------------------------------------------------------+
 */
 
-define('RRD_NL', ' \\\n');
+define('RRD_NL', " \\\n");
 define('MAX_FETCH_CACHE_SIZE', 5);
 
 define('RRD_FILE_VERSION1', '0001');
