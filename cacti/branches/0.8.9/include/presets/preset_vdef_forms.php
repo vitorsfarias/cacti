07afffd483ec48ad457ba49085a738139123754c
gandalf <gandalf@860744bd-22fc-0310-8c96-e9fe5004b5ca>
2012-05-09 18:51:06 +0000

    required includes

---
+++ cacti/branches/0.8.9/include/presets/preset_vdef_forms.php
@@ -0,0 +1,40 @@
+<?php
+/*
+ +-------------------------------------------------------------------------+
+ | Copyright (C) 2004-2012 The Cacti Group                                 |
+ |                                                                         |
+ | This program is free software; you can redistribute it and/or           |
+ | modify it under the terms of the GNU General Public License             |
+ | as published by the Free Software Foundation; either version 2          |
+ | of the License, or (at your option) any later version.                  |
+ |                                                                         |
+ | This program is distributed in the hope that it will be useful,         |
+ | but WITHOUT ANY WARRANTY; without even the implied warranty of          |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           |
+ | GNU General Public License for more details.                            |
+ +-------------------------------------------------------------------------+
+ | Cacti: The Complete RRDTool-based Graphing Solution                     |
+ +-------------------------------------------------------------------------+
+ | This code is designed, written, and maintained by the Cacti Group. See  |
+ | about.php and/or the AUTHORS file for specific developer information.   |
+ +-------------------------------------------------------------------------+
+ | http://www.cacti.net/                                                   |
+ +-------------------------------------------------------------------------+
+*/
+
+/* file: vdef.php, action: edit */
+$fields_vdef_edit = array(
+	'name' => array(
+		'method' => 'textbox',
+		'friendly_name' => __('Name'),
+		'description' => __('A useful name for this VDEF.'),
+		'value' => '|arg1:name|',
+		'max_length' => '255',
+	),
+);
+
+$fields_vdef_item_edit = array(
+	'sequence' => 'sequence',
+	'type' => 'type',
+	'value' => 'value'
+);
