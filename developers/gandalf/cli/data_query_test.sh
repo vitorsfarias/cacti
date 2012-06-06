11984e48c3d4084dc231f3299f057f92b4e53bb5
gandalf <gandalf@860744bd-22fc-0310-8c96-e9fe5004b5ca>
2012-06-06 15:39:31 +0000

    testing procedures for new cli code

--- developers/gandalf/cli/data_query_test.sh
+++ developers/gandalf/cli/data_query_test.sh
@@ -2,6 +2,7 @@
 php -q data_query_list.php --help
 php -q data_query_list.php
 php -q data_query_list.php --data-query-id=1
+# assume, that a host using SNMP version 2 is present (e.g. localhost changed to SNMP V2c)
 php -q data_query_list.php --version=2
 
 # add and remove
@@ -20,10 +21,12 @@ php -q data_query_list.php --device-id=1
 # update
 clear
 php -q data_query_update.php --help
+php -q data_query_add.php --device-id=1 --data-query-id=1 --reindex-method=uptime
 php -q data_query_update.php  --data-query-id=1 --reindex-method=:value --version=2 -d
 php -q data_query_update.php  --data-query-id=1 --reindex-method=:value --version=2
 php -q data_query_list.php --data-query-id=1
 
+php -q data_query_add.php --device-id=1 --data-query-id=6 --reindex-method=uptime
 php -q data_query_update.php  --data-query-id=6 --reindex-method=:3 --version=2 -d
 php -q data_query_update.php  --data-query-id=6 --reindex-method=:3 --version=2
 php -q data_query_list.php --data-query-id=6
--- developers/gandalf/cli/perms_test.sh
+++ developers/gandalf/cli/perms_test.sh
@@ -2,16 +2,19 @@ echo -----------------------
 echo  list options
 echo -----------------------
 php -q perms_list.php --help
-php -q perms_list.php --list-groups
+#php -q perms_list.php --list-groups
 php -q perms_list.php --list-users
 php -q perms_list.php --list-trees
 php -q perms_list.php --list-realms
+# user "admin" should always exist
 php -q perms_list.php --list-realms --user-id=1
 php -q perms_list.php --list-realms --realm-id=7
+# plugin management realm should always exist
 php -q perms_list.php --list-realms --user-id=1 --realm-id=101
 php -q perms_list.php --list-perms
 php -q perms_list.php --list-perms --user-id=1
 php -q perms_list.php --list-perms --item-type=graph
+# make sure, that a graph exists: 54
 php -q perms_list.php --list-perms --item-id=54
 php -q perms_list.php --list-perms --user-id=1 --item-type=graph
 echo This will throw errors
@@ -23,40 +26,18 @@ echo -----------------------
 echo add and remove perms
 echo -----------------------
 php -q perms_create.php --help
-php -q perms_create.php --item-type=graph --item-id=54 --user-id=3,1
+# get a valid graph id and replace here: 54
+php -q perms_create.php --item-type=graph --user-id=3,1 --item-id=54
 php -q perms_list.php --list-perms --item-type=graph
-php -q perms_delete.php --item-type=graph --item-id=54 --user-id=3,1
+php -q perms_delete.php --item-type=graph --user-id=3,1 --item-id=54
 php -q perms_list.php --list-perms --item-type=graph
 
-php -q perms_create.php --item-type=device --item-id=5 --user-id=3,1
+php -q perms_create.php --item-type=device --user-id=3,1 --item-id=1
 php -q perms_list.php --list-perms --item-type=device
-php -q perms_delete.php --item-type=device --item-id=5 --user-id=3,1
+php -q perms_delete.php --item-type=device --user-id=3,1 --item-id=1
 php -q perms_list.php --list-perms --item-type=device
 
-php -q perms_create.php --item-type=graph_template --item-id=2 --user-id=3,1
+php -q perms_create.php --item-type=graph_template --user-id=3,1 --item-id=2
 php -q perms_list.php --list-perms --item-type=graph_template
-php -q perms_delete.php --item-type=graph_template --item-id=2 --user-id=3,1
+php -q perms_delete.php --item-type=graph_template --user-id=3,1 --item-id=2
 php -q perms_list.php --list-perms --item-type=graph_template
-
-
-clear
-echo -----------------------
-echo update
-echo -----------------------
-php -q perms_create.php --perms-type=cg --device-id=1 --perms-template-id=11
-php -q perms_list.php --perms-id=49
-
-php -q perms_update.php --help
-php -q perms_update.php --perms-id=49 --perms-title='New Title1'
-php -q perms_list.php --perms-id=49
-
-php -q perms_create.php --perms-type=cg --device-id=1 --perms-template-id=11 --force
-php -q perms_list.php --perms-template-id=11
-php -q perms_update.php --perms-template-id=11 --perms-title='New Title2'
-php -q perms_list.php --perms-template-id=11
-
-php -q perms_update.php --device-id=1 --perms-template-id=11 --perms-title='New Title3'
-php -q perms_list.php --device-id=1 --perms-template-id=11
-
-php -q perms_delete.php --device-id=1 --perms-template-id=11 --force
-php -q perms_list.php --device-id=1
