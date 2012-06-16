54e0df1bbe9caf568f0982a049d66806288f6ba8
gandalf <gandalf@860744bd-22fc-0310-8c96-e9fe5004b5ca>
2012-06-16 20:37:57 +0000

    need layout file for new UI

--- cacti/branches/0.8.9/include/top_header.php
+++ cacti/branches/0.8.9/include/top_header.php
@@ -49,9 +49,10 @@ $page_title = plugin_hook_function('page_title', draw_navigation_text("title"));
 	<link type='text/css' media='screen' href='<?php echo CACTI_URL_PATH; ?>include/css/cacti_dd_menu.css' rel='stylesheet'>
 	<link type='text/css' media='screen' href='<?php echo CACTI_URL_PATH; ?>include/css/jquery-ui.all.css' rel='stylesheet'>
 	<link href='<?php echo CACTI_URL_PATH; ?>images/favicon.ico' rel='shortcut icon'>
-	<script type="text/javascript" src="<?php echo CACTI_URL_PATH; ?>include/layout.js"></script>
+	<script type='text/javascript' src='<?php echo CACTI_URL_PATH; ?>include/layout.js'></script>
 	<script type='text/javascript' src='<?php echo CACTI_URL_PATH; ?>include/js/jquery/jquery.js'></script>
 	<script type='text/javascript' src='<?php echo CACTI_URL_PATH; ?>include/js/jquery/jquery-ui.js'></script>
+	<script type='text/javascript' src='<?php echo CACTI_URL_PATH; ?>include/layout.php'></script>
 	<script type='text/javascript' src='<?php echo CACTI_URL_PATH; ?>include/js/jquery/jquery.tablednd.js'></script>
 	<script type='text/javascript' src='<?php echo CACTI_URL_PATH; ?>include/js/jquery/jquery.dropdown.js'></script>
 	<script type='text/javascript' src='<?php echo CACTI_URL_PATH; ?>include/js/jquery/jquery.dd.js'></script>
