def9cbe1c48d885c4bf806145022e0c137d5a93a
gandalf <gandalf@860744bd-22fc-0310-8c96-e9fe5004b5ca>
2012-10-03 18:09:25 +0000

    new test script

---
+++ developers/gandalf/cli/poller_graphs_reapply_names.sh
@@ -0,0 +1,5 @@
+php -q poller_graphs_reapply_names.php --help
+php -q poller_graphs_reapply_names.php -id=All
+php -q poller_graphs_reapply_names.php -id=all
+php -q poller_graphs_reapply_names.php -id=otto
+php -q poller_graphs_reapply_names.php -id="1|2|3|4" -d
