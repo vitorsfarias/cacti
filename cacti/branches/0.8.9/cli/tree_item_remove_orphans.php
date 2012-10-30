21c0c48dbce6ac02a06bd0f0777bd28e83cdd302
gandalf <gandalf@860744bd-22fc-0310-8c96-e9fe5004b5ca>
2012-10-30 17:12:10 +0000

    - fix issues when searching orphaned headers across multiple trees - add debug option - add option to run against all trees

--- cacti/branches/0.8.9/cli/tree_item_remove_orphans.php
+++ cacti/branches/0.8.9/cli/tree_item_remove_orphans.php
@@ -2,7 +2,7 @@
 <?php
 /*
  +-------------------------------------------------------------------------+
- | Copyright (C) 2004-2012 The Cacti Group                                 |
+ | Copyright (C) 2004-2011 The Cacti Group                                 |
  |                                                                         |
  | This program is free software; you can redistribute it and/or           |
  | modify it under the terms of the GNU General Public License             |
@@ -30,38 +30,44 @@ if (!isset($_SERVER["argv"][0]) || isset($_SERVER['REQUEST_METHOD'])  || isset($
 
 $no_http_headers = true;
 
-include(dirname(__FILE__) . "/../include/global.php");
-include_once(CACTI_LIBRARY_PATH . "/automation_tools.php");
+include(dirname(__FILE__)."/../include/global.php");
+include_once(CACTI_LIBRARY_PATH . "/api_automation_tools.php");
 include_once(CACTI_LIBRARY_PATH . '/tree.php');
+include_once(CACTI_LIBRARY_PATH . '/api_tree.php');
 
 /* process calling arguments */
 $parms = $_SERVER["argv"];
 array_shift($parms);
 
-$treeId     = 0;   # When creating a node, it has to go in a tree
+$treeId     = 0;
 $remove = FALSE;
+$debug = FALSE;
 
 if (sizeof($parms)) {
 	foreach($parms as $parameter) {
 		@list($arg, $value) = @explode("=", $parameter);
-
+		
 		switch ($arg) {
-		case "--tree-id":
-			$treeId = $value;
-
-			break;
-		case "--remove":
-			$remove = TRUE;
-
-			break;
-		case "--version":
-		case "-V":
-		case "-H":
-		case "--help":
-			display_help();
-			exit(0);
-		default:
-			echo "ERROR: Invalid Argument: ($arg)\n\n";
+			case "--tree-id":
+				$treeId = $value;
+				
+				break;
+			case "--remove":
+				$remove = TRUE;
+				
+				break;
+			case "--debug":
+				$debug = TRUE;
+				
+				break;
+			case "--version":
+			case "-V":
+			case "-H":
+			case "--help":
+				display_help();
+				exit(0);
+			default:
+				echo "ERROR: Invalid Argument: ($arg)\n\n";
 			display_help();
 			exit(1);
 		}
@@ -71,45 +77,84 @@ if (sizeof($parms)) {
 	 * order matters!
 	 * fetch deepest level of headers
 	 * in case you remove a deeper header, the higher level may become orphaned as well! */
+	if (strtolower($treeId) === "all") {
+		$sql = "SELECT * FROM graph_tree ORDER BY id";
+	} else {
+		$sql = "SELECT * FROM graph_tree WHERE id=$treeId ORDER BY id";
+	}
+	
+	$tree_array = db_fetch_assoc($sql);
+	if (sizeof($tree_array)) {
+		foreach ($tree_array as $tree) {
+			if ( $debug ) {
+				print "Working on " . $tree['name'] . "[" . $tree['id'] . "]\n";
+			}
+			remove_orphans($tree['id'], $remove, $debug);
+		}
+	} else {
+		print "No matching tree found\n";
+		exit(1);
+	}
+} else {
+	display_help();
+}
+
+
+function remove_orphans($treeId, $remove, $debug) {
 	$sql = "SELECT id, title, order_key FROM graph_tree_items WHERE graph_tree_id=$treeId AND host_id=0 AND local_graph_id=0 ORDER BY order_key DESC";
 	$header_array = db_fetch_assoc($sql);
-
+	
 	/* research headers */
 	if (sizeof($header_array)) {
 		foreach ($header_array as $header) {
-			/* extract relevant part of order_key */
+		/* extract relevant part of order_key */
 			$tier = tree_tier($header["order_key"]);
 			$resulting_order_key = substr($header["order_key"], 0, ($tier * CHARS_PER_TIER));
+			if ( $debug ) {
+				print "TREE[" . $treeId . "] " . $header['title'] . "[" . $header['id'] . "]: " . $resulting_order_key;
+			}
 			
 			/* find children of resulting order_key */
-			$sql = "SELECT COUNT(*) FROM graph_tree_items WHERE order_key LIKE '" . $resulting_order_key . "%%'";
+			$sql = "SELECT COUNT(*) FROM graph_tree_items WHERE graph_tree_id=$treeId AND order_key LIKE '" . $resulting_order_key . "%%'";
 			$children = db_fetch_cell($sql);
+			if ( $debug ) {
+				print " - " . $children . "\n";
+			}
+			
 			/* in case we only find the header itself, it's an orphaned header */
 			if ($children <= 1) {
-				print "Orphaned header: " . $header["title"];
+				print "TREE[" . $treeId . "] " . "Orphaned header: " . $header["title"];
 				if ($remove) {
-					/* remove stale header */
-					db_execute("DELETE FROM graph_tree_items WHERE id = " . $header["id"]);
+				/* remove stale header */
+					db_execute("DELETE FROM graph_tree_items WHERE graph_tree_id=$treeId AND id = " . $header["id"]);
 					print " deleted\n";
 				}else{
 					print "\n";
 				}
 			}else{
+				if ( $debug ) {
+					$sql = "SELECT * FROM graph_tree_items WHERE graph_tree_id=$treeId AND order_key LIKE '" . $resulting_order_key . "%%'";
+					$child_array = db_fetch_assoc($sql);
+					if (sizeof($child_array)) {
+						foreach($child_array as $child) {
+							print "TREE[" . $treeId . "] " . $header['title'] . "[" . $child['id'] . "]: " . $child['order_key'] . " title: " . $child['title'] . " graph: " . $child['local_graph_id'] . " host: " . $child['host_id'] . "\n";
+							
+						}
+					}
+				}
 				/* NOP, non-orphaned header */
 			}
 		}
 	}
-} else {
-	display_help();
-	exit(0);
 }
 
 function display_help() {
-	echo "Remove Stale Tree Headers Script 1.0, Copyright 2004-2012 - The Cacti Group\n\n";
+	echo "Remove Stale Tree Headers Script 1.1, Copyright 2004-2012 - The Cacti Group\n\n";
 	echo "A simple command line utility to remove orphaned tree headers in Cacti\n\n";
-	echo "usage: tree_item_remove_orphans.php.php  --tree-id=[tree id] [--remove] [--quiet]\n\n";
-	echo "    --tree-id=[ID]    id of tree\n";
+	echo "usage: tree_item_remove_orphans.php.php  --tree-id=[tree id] [--remove] [--debug]\n\n";
+	echo "    --tree-id=[ID]    id of tree or 'all' for all trees\n";
 	echo "    --remove          remove orphaned header entries\n";
+	echo "    --debug           print lots of messages\n";
 }
 
 ?>
