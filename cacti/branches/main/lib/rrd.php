3667e32fb8cbd0a4378e75e5b450c623ee8567b4
cigamit <cigamit@860744bd-22fc-0310-8c96-e9fe5004b5ca>
2014-08-18 14:37:42 +0000

    Slight addition to bug #2455.  We corrected the SQL Inject, now we force one of the default font sizes if its not a numeric value.

--- cacti/branches/main/lib/rrd.php
+++ cacti/branches/main/lib/rrd.php
@@ -3279,10 +3279,10 @@ function rrdtool_set_font($type, $no_legend = "") {
 	if ($type == "title") {
 		if (!empty($no_legend)) {
 			$size = $size * .70;
-		}elseif (($size <= 4) || ($size == "")) {
+		}elseif (($size <= 4) || !is_numeric($size)) {
 			$size = 12;
 		}
-	}else if (($size <= 4) || ($size == "")) {
+	}else if (($size <= 4) || !is_numeric($size)) {
 		$size = 8;
 	}
 
