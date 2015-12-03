830275529b2f001dcfa699c01a7ca840b5494bab
cigamit <cigamit@860744bd-22fc-0310-8c96-e9fe5004b5ca>
2015-12-03 02:38:58 +0000

    completing backport to legaqcy database.php

--- cacti/tags/0.8.8g/lib/database.php
+++ cacti/tags/0.8.8g/lib/database.php
@@ -321,7 +321,7 @@ function sql_save($array_items, $table_name, $key_cols = "id", $autoinc = TRUE, 
 	}
 
 	while (list($key, $value) = each($array_items)) {
-		$array_items[$key] = sql_sanitize($value);
+		$array_items[$key] = "\"" . sql_sanitize($value) . "\"";
 	}
 
 	$replace_result = $db_conn->Replace($table_name, $array_items, $key_cols, FALSE, $autoinc);
@@ -335,7 +335,7 @@ function sql_save($array_items, $table_name, $key_cols = "id", $autoinc = TRUE, 
 	if (($db_conn->Insert_ID() == "0") || ($replace_result == 1)) {
 		if (!is_array($key_cols)) {
 			if (isset($array_items[$key_cols])) {
-				return str_replace("'", "", $array_items[$key_cols]);
+				return str_replace("\"", "", $array_items[$key_cols]);
 			}
 		}
 
